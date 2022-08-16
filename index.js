a29aa4bcbe61563d20a78ea10734e49febb33da7
Muriithi <muriithi.kibata@student.moringaschool.com>
2022-08-16 07:57:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 6.2
+let num2 = 10
+let multiply = num1 * num2
+
+let random = 10
+//Math.random(random)
+Math.floor(Math.random() * 10) 
+let num3 = 44
+let num4 = 10
+ let mod = num3 % num4
+ let max = 20
+ let here = 18
+ Math.max(here, max)
